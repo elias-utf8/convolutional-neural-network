8d8ab6258b516e071a3aab026489b2220386ad5e
Elias Gauthier <gauthier.elias016@gmail.com>
2025-02-02 21:54:59 +0100

    Premier commit

---
+++ model_application.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing import image
+
+model = tf.keras.models.load_model('walter.h5')
+
+cifar10_classes = [
+    'avion', 'automobile', 'oiseau', 'chat', 'cerf',
+    'chien', 'grenouille', 'cheval', 'bateau', 'camion'
+]
+
+def preprocess_image(image_path, target_size=(32, 32)):
+    img = image.load_img(image_path, target_size=target_size)
+    img_array = image.img_to_array(img)
+    img_array = img_array / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    return img_array
+
+def predict_image(image_path):
+    img_array = preprocess_image(image_path)
+    predictions = model.predict(img_array)
+    predicted_class_index = np.argmax(predictions, axis=1)[0]
+    predicted_class = cifar10_classes[predicted_class_index]
+    confidence = predictions[0][predicted_class_index]
+    return predicted_class, confidence
+
+image_path = 'avion.png'
+
+predicted_class, confidence = predict_image(image_path)
+print(f"Je pense que cette image est : {predicted_class} avec une confiance de {confidence:.2f}")
